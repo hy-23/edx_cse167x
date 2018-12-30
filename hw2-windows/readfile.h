abe505e3e2dd0ef5c4093d15674feedba60e11b1
hy23 <harsha.yogeshappa@gmail.com>
2018-12-30 21:53:24 +0530

    Adding files
    Currently hw1.txt file is used to parse and tea pot is seen.
    Shader needs to be enabled, and hence the color and lighting.

---
+++ hw2-windows/readfile.h
@@ -0,0 +1,11 @@
+#include <string>
+#include <stack>
+#include <sstream>
+
+// Readfile definitions
+
+void matransform(std::stack<mat4> &transfstack, GLfloat * values);
+void rightmultiply(const mat4 & M, std::stack<mat4> &transfstack);
+bool readvals(std::stringstream &s, const int numvals, GLfloat * values);
+void readfile(const char * filename);
+void parseLine(const std::string &str, std::stack<mat4> &transfstack);
